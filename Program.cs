d847b5827442803e7a09c1e52f84639fa1de2f6d
Shubham Mule <shubhambmule@gmail.com>
2021-10-25 15:51:23 +0530

    Added : Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure Problems");
+        }
+    }
+}
