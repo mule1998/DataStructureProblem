61c0f7f200a7d766452f0b8f4c5513ed9034d62c
Shubham Mule <shubhambmule@gmail.com>
2021-10-25 16:21:24 +0530

    Added : Binary Search Word

---
+++ DataStructure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class DataStructure
+    {
+        public int FindWord(string[] words, string str)
+        {
+            int start = 0;
+            int end = words.Length - 1;
+            while (start <= end)
+            {
+                int mid = start + end / 2;
+                int result = str.CompareTo(words[mid]);
+                if (result == 0)
+                {
+                    return mid;
+                }
+                if (result > 0)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace DataStructureProblem
+namespace DataStructures
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Data Structure Problems");
+            Console.WriteLine("Data Structure!");
+            DataStructure dataStructure = new DataStructure();
+            string[] words = { "decs", "read", "logic", "search" };
+            string data = "search";
+            int result = dataStructure.FindWord(words, data);
+            if (result == -1)
+                Console.WriteLine("Word not present");
+
+            else
+                Console.WriteLine("Word is found at " + result);
         }
     }
 }
